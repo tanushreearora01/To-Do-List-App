2cf06d00dcdaac48c3a3fc4d12402fa0810f304d
Tanushree Arora <aroratanushree.28@gmail.com>
2024-03-27 00:49:26 -0400

    Initial Commit

---
+++ To-Do List App/To-Do List App/To_Do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_AppApp.swift
+//  To-Do List App
+//
+//  Created by Tanushree Arora on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
